0b5b2a09ff394fcc6a1cda3905b185ed67099f62
Thomas Phillips <fatesunited@gmail.com>
2026-03-08 21:26:59 -0500

    fixed form functionality to include QoL changes and increased usefulness. also fixed how data was displayed on the details and index page

--- main_app/models.py
+++ main_app/models.py
@@ -1,21 +1,49 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+CORPORATIONS = (
+    ('TL', 'Tidal Lock'),
+    ('VL', 'Vapor Lock.')
+)
+
+TIMEZONES = (
+    ('US', 'US'),
+    ('EU', 'EU'),
+    ('AU', 'AU')
+)
+
+MEMBER_STATES = (
+    ('Recruit', 'Recruit'),
+    ('Member', 'Member')
+)
+
+STATUS_CHOICES = (
+    ('Pending', 'Pending'),
+    ('Recruit', 'Recruit'),
+    ('Member', 'Member'),
+    ('Purged', 'Purged'),
+    ('Rejected', 'Rejected'),
+    ('Declined', 'Declined'),
+    ('Left', 'Left'),
+    ('Kicked', 'Kicked')
+)
 
 class Member(models.Model):
     username = models.CharField(max_length=100, unique=True)
-    timezone = models.CharField(max_length=100, blank=True, null=True)
-    interviewed_by = models.IntegerField(blank=True, null=True)
-    esi_check_by = models.IntegerField('ESI checked by',blank=True, null=True)
-    onboarded_by = models.IntegerField(blank=True, null=True)
-    corporation = models.CharField(max_length=100, blank=True, null=True)
+    timezone = models.CharField(max_length=100, choices=TIMEZONES, default=TIMEZONES[0][0], blank=True, null=True)
+    interviewed_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='interviewed_members', blank=True, null=True)
+    esi_checked_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='esi_checked_members', blank=True, null=True)
+    onboarded_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='onboarded_members', blank=True, null=True)
+    corporation = models.CharField(max_length=100, choices=CORPORATIONS, default=CORPORATIONS[0][0], blank=True, null=True)
     join_date = models.DateField(blank=True, null=True)
     notes = models.TextField(max_length=500, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    member_state = models.CharField(max_length=100, blank=True, null=True)
-    num_of_characters = models.IntegerField('Number of characters', blank=True, null=True)
+    member_state = models.CharField(max_length=100, choices=MEMBER_STATES, default=MEMBER_STATES[0][0], blank=True, null=True)
+    num_of_characters = models.IntegerField(blank=True, null=True)
     registry_number = models.CharField(max_length=100, blank=True, null=True)
-    current_status = models.CharField(max_length=100, blank=True, null=True)
+    current_status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     graduation_date = models.DateField(blank=True, null=True)
     attrition_headcount = models.IntegerField(blank=True, null=True)
